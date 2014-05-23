aa0c5ee089ed9000445f2f6c2a208c8982d62bf4
Douglas Gardner <douglas@chippy.ch>
2014-05-23 01:36:15 +0100

    +

--- run.sh
+++ run.sh
@@ -10,11 +10,12 @@ fi
 if [ -n "$WERCKER_UDP_PORT" ] ; then
 	cmd="$cmd $WERCKER_UDP_PORT"
 else
-	cmd-"$cmd 3000"
+	cmd="$cmd 3000"
 fi
 
 message="[wercker] $WERCKER_APPLICATION_OWNER_NAME/$WERCKER_APPLICATION_NAME:"
-message="$message build $WERCKER_RESULT ($WERCKER_STARTED_BY on $WECKER_GIT_BRANCH"
+message="$message build $WERCKER_RESULT ($WERCKER_STARTED_BY on $WECKER_GIT_BRANCH)"
 
+echo "Sending $message"
 echo $message > $cmd
 echo "Sent $WERCKER_RESULT message to $WERCKER_UDP_HOST:$WERCKER_UDP_PORT"
